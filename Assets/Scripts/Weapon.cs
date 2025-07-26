2d21033b34730aa4aa3a4a7700d1c7e418ec7962
Validate.exe <159242315+skeloop@users.noreply.github.com>
2025-07-27 00:31:59 +0200

    kek

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,23 +2,25 @@
 
 namespace Core
 {
-	[CreateAssetMenu(fileName = "MechWeapon", menuName = "Weapon", order = 0)]
 	public abstract class Weapon : ScriptableObject
 	{
+		[Range(0,100)]
 		public float Damage;
 		public float ShootDelay;
 	}
 
+	[CreateAssetMenu(fileName = "MechWeapon", menuName = "Laser Weapon", order = 0)]
 	public class Laser : Weapon
 	{
 		
 	}
-
+	[CreateAssetMenu(fileName = "MechWeapon", menuName = "Gun Weapon", order = 0)]
 	public class Projectile : Weapon
 	{
 		
 	}
 
+	[CreateAssetMenu(fileName = "MechWeapon", menuName = "Missile Launcher", order = 0)]
 	public class Missile : Weapon
 	{
 		
